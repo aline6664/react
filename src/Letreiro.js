64b447f2e3a0adf4d890f68968dd8734ab2eb462
aline6664 <gley.line@gmail.com>
2025-04-15 12:24:59 -0300

    ADD lógicas de Relogio.js e Letreiro.js

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,12 +1,40 @@
+import { useEffect, useState } from 'react';
 import './Letreiro.css';
 
-function App() {
+function MeuTexto({digitado}) {
+    return <><h1>{digitado}</h1></>
+}
+
+function Letreiro() {
+    const textoCompleto = "Conheça a Fatec!";
+    const [textoDigitado, setDigitado] = useState("");
+    const [i, setIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalo = setInterval(() => {
+            // Enquanto não atingir o final da frase, digitar
+            if (i <= textoCompleto.length) {
+                setDigitado(textoCompleto.slice(0, i));
+                setIndex((prev) => prev + 1);
+            }
+            // Se chegar ao final, realizar o looping
+            // Reseta o texto digitado e o índice
+            else {
+                setTimeout(() => {
+                setDigitado("");
+                setIndex(0);
+            }, 1000); // 1 segundo de pausa antes de recomeçar
+            }
+        }, 200); // 0.2s por letra
+    
+        return () => clearInterval(intervalo); // limpeza do intervalo a cada re-render
+      }, [i]);
+
     return(
         <>
-            <h1>Letreiro</h1>
-            <p>Meu letreiro</p>
+            <MeuTexto digitado = {textoDigitado}/>
         </>
     );
 }
 
-export default App;
+export default Letreiro;
