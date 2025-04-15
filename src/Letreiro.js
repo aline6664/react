2b7db305dfc57f37043d43e15b87cd02f32f3c63
Aline <136123182+aline6664@users.noreply.github.com>
2025-04-15 11:26:03 -0300

    ADD arquivos Create React App

---
+++ src/Letreiro.js
@@ -0,0 +1,12 @@
+import './Letreiro.css';
+
+function App() {
+    return(
+        <>
+            <h1>Letreiro</h1>
+            <p>Meu letreiro</p>
+        </>
+    );
+}
+
+export default App;
---
+++ src/Relogio.js
@@ -0,0 +1,12 @@
+import './Relogio.css';
+
+function App() {
+    return(
+        <>
+            <h1>Relógio</h1>
+            <p>Meu relógio</p>
+        </>
+    );
+}
+
+export default App;
