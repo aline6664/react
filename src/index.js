a62bdb99807692b766c92e8f5e0f43e7adb385a1
aline6664 <gley.line@gmail.com>
2025-04-22 11:51:49 -0300

    UPDATE index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Relogio from './Relogio';
-import Letreiro from './Letreiro';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Relogio />
-    <Letreiro />
-    <App />
-    <App />
     <App />
   </>
 );
