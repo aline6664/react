9c28db0b012019651aeb66ee2c861694dcb6c297
aline6664 <gley.line@gmail.com>
2025-04-30 21:11:21 -0300

    FINALIZE Contador.Js

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -39,14 +39,11 @@ function ContadorBtnRemover({ onClick }) {
     );
 }
 
-function ContadorPessoa( { genero, icon }) { // passando props da array pessoa    
-    const [valor, setValor] = useState(0);
-
+function ContadorPessoa( { genero, icon, valor, mudarValor }) { // passando props da array pessoa   
     // funções que serão triggered ao clicar nos botões
-    // os componentes chamam eles pelo onClick
-    const adicionar = () => setValor(v => v + 1);
-    const remover = () => setValor(v => v > 0 ? v - 1 : 0);
-
+    // os componentes chamam eles pelo onClick 
+    const adicionar = () => mudarValor(valor + 1); // mudarValor() chama atualizarValor(index, novo)
+    const remover = () => mudarValor(valor > 0 ? valor - 1 : 0); // ou seja, ele pede para o pai atualizar o state
     return (
         <div>
             <img src={icon} className="icon-pessoa"/>
@@ -60,42 +57,70 @@ function ContadorPessoa( { genero, icon }) { // passando props da array pessoa
     );
 }
 
-function ContadorBtnReset() {
+function ContadorBtnReset({ onClick }) {
     return (
-        <button className="icon-resetar"><img src={iconReset} id="reset"/></button>
+        <button onClick={onClick} className="icon-resetar">
+            <img src={iconReset} id="reset"/>
+        </button>
     );
 }
 
 function ContadorTitulo() {
     return (
-            <h1 id="total">Total</h1>
+        <h1 id="total">Total</h1>
     );
 }
-function ContadorTabela() {
-    // cria as colunas baseado na array pessoa
+function ContadorTabela( {valores, atualizarValor}) {
+    // cria as colunas baseado na array pessoa (index 0 - homens, 1 - mulheres)
     // utiliza o map() para passar pelo array todo e criar uma funcao de retorno em cada elemento
     // nesse caso ele retorna o genero (Homens/Mulheres) e os icons
+    // tambem esta passando o valor e o valor atualizado recebido como props pelo ContadorContainer
     return (
         <div id="tabela">
           {pessoa.map((p, index) => (
-            <ContadorPessoa key={index} genero={p.genero} icon={p.icon} />
+            <ContadorPessoa
+                key={index}
+                genero={p.genero}
+                icon={p.icon}
+                valor={valores[index]} // valores para cada genero
+                mudarValor={(novoValor) => atualizarValor(index, novoValor)} // para atualizar o valor desse index
+            />
           ))}
         </div>
     )
 }
 export default function ContadorContainer() {
+    const [valores, setValores] = useState([0, 0]); // array que vale para cada gênero
+
+    // funcao para atualizar valor de um genero especifico
+    // index 0 - homens, 1 - mulheres
+    // map() cria um novo array com o novo valor do index que foi mudado, deixando o outro inalterado
+    const atualizarValor = (index, novoValor) => {
+        setValores(valores.map((valor, i) => i === index ? novoValor : valor));
+    };
+
+    // calcula a soma de todos elementos no array valores
+    // reduce() passa pelo array todo e realiza a funcao especificada
+    // nesse caso, realiza soma de cada elemento, comecando pelo index 0
+    const total = valores.reduce((a, b) => a + b, 0);
+
+    const resetar = () => setValores([0, 0]);
+
     return (
         <div id="conteudo">
-            <ContadorBtnReset />
+            <ContadorBtnReset onClick={resetar} />
             <ContadorTitulo />
-            <ContadorValorTotal />
-            <ContadorTabela />
+            <ContadorValorTotal valorTotal={total}/>
+            <ContadorTabela 
+                valores={valores}
+                atualizarValor={atualizarValor}
+            />
         </div>
     );
 }
 
 // Homens e Mulheres
 const pessoa = [
-    {genero: "Homens", icon: iconHomem}, // nota: sem {} pois nãoo queremos um object mas um path
+    {genero: "Homens", icon: iconHomem}, // nota: sem {} pois não queremos um object mas um path
     {genero: "Mulheres", icon: iconMulher}
 ];
